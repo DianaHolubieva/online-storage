2fa5914b43c5f99fbf4ae31bf911dcdc58946123
DianaHolubieva <golubevadiana356@gmail.com>
2022-06-22 18:43:25 +0200

    was added models db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 const PORT = process.env.PORT || 5000;
+const  models = require('./models/models');
+
 const app = express();
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+const start = async () => {
+    try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    }catch (e){
+        console.log(e);
+    }
+}
+
+start();
+
